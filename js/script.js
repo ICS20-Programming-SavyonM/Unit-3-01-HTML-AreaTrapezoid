d027ea254faff9137542082d3d5ffb941cb23422
SavyonM <124379798+SavyonMalko@users.noreply.github.com>
2023-03-29 21:26:40 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
-function calculate() {
-  const a = Number(document.getElementById("a").value);
-  const b = Number(document.getElementById("b").value);
-  const area = Math.PI * a * b;
-  const circumference = 2 * Math.PI * Math.sqrt((a * a + b * b) / 2);
-  const result = `Area: ${area.toFixed(2)}m2 Circumference: ${circumference.toFixed(2)}m2`;
-  document.getElementById("result").innerHTML = result;
+// Copyright (c) 2023 Savyon All rights reserved
+//
+// Created by: Savyon
+// Created on: March 2022
+// This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates the area and perimeter of rectangle.
+ */
+function calcAreaClicked () {
+  // get user input
+  let side_a = parseFloat(document.getElementById('side-a').value)
+  let side_b = parseFloat(document.getElementById('side-b').value)
+	let height = parseFloat(document.getElementById('height').value)
+
+
+  // calculate the area
+  let area = (side_a + side_b) / 2 * height 
+
+  // display the results
+  document.getElementById('area').innerHTML = "The area is " + area.toFixed(2) + "cm<sup>2</sup>"
 }
